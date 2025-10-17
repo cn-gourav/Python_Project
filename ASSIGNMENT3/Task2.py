14dd67c41778b36ab4e91540a43784fe02731fd5
cn-gourav <mrgusain558@gmail.com>
2025-10-18 03:01:31 +0530

    Complete Project3 task completed

---
+++ ASSIGNMENT3/Task2.py
@@ -0,0 +1,11 @@
+import math
+a = int(input("Enter a number: "))
+
+# square number 
+print("Square root: ",math.sqrt(a))
+
+# Logarithm 
+print("Logarithm: ",math.log(a))
+
+# Trignometric sin
+print("Sine: ",math.sin(a))
